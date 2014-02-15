369b0c6e0ad855f4d5b02bb58d39586c58e7ecb3
bren525 <bren525@gmail.com>
2014-02-15 14:37:12 -0500

    Controller throttle test

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -1,1 +1,61 @@
 /*Arduino Control Code*/
+//motor A connected between A01 and A02
+
+int STBY = 10; //standby
+
+//Motor A
+int PWMA = 3; //Speed control 
+int AIN1 = 9; //Direction
+int AIN2 = 8; //Direction
+
+int throttle;
+int pct;
+
+void setup(){
+  Serial.begin(115200);
+  pinMode(STBY, OUTPUT);
+
+  pinMode(PWMA, OUTPUT);
+  pinMode(AIN1, OUTPUT);
+  pinMode(AIN2, OUTPUT);
+}
+void loop(){
+  throttle = analogRead(A0);
+  pct = throttle/1023;
+  Serial.println(throttle);
+  Serial.println(pct);
+  move(1,255*pct,1);
+  
+  delay(5);
+  stop();
+  
+}
+
+void move(int motor, int speed, int direction){
+//Move specific motor at speed and direction
+//motor: 0 for B 1 for A
+//speed: 0 is off, and 255 is full speed
+//direction: 0 clockwise, 1 counter-clockwise
+
+  digitalWrite(STBY, HIGH); //disable standby
+
+  boolean inPin1 = LOW;
+  boolean inPin2 = HIGH;
+
+  if(direction == 1){
+    inPin1 = HIGH;
+    inPin2 = LOW;
+  }
+
+  if(motor == 1){
+    digitalWrite(AIN1, inPin1);
+    digitalWrite(AIN2, inPin2);
+    analogWrite(PWMA, speed);
+  }else{
+  }
+}
+
+void stop(){
+//enable standby  
+  digitalWrite(STBY, LOW); 
+}
--- motor/motor.ino
+++ motor/motor.ino
@@ -1,32 +1,72 @@
-float pct = 0;
-int throttle = 0;
+//motor A connected between A01 and A02
+//motor B connected between B01 and B02
 
-void setup() {
-  Serial.begin(115200);
-  //Setup Channel A
-  pinMode(8, OUTPUT); //Initiates Motor Channel A pin
-  pinMode(11, OUTPUT);  //Initiates Brake Channel A pin
-  pinMode(A0, INPUT);  //Initiates Throttle Input
+int STBY = 10; //standby
+
+//Motor A
+int PWMA = 3; //Speed control 
+int AIN1 = 9; //Direction
+int AIN2 = 8; //Direction
+
+//Motor B
+int PWMB = 5; //Speed control
+int BIN1 = 11; //Direction
+int BIN2 = 12; //Direction
+
+void setup(){
+  pinMode(STBY, OUTPUT);
+
+  pinMode(PWMA, OUTPUT);
+  pinMode(AIN1, OUTPUT);
+  pinMode(AIN2, OUTPUT);
+
+  pinMode(PWMB, OUTPUT);
+  pinMode(BIN1, OUTPUT);
+  pinMode(BIN2, OUTPUT);
 }
 
-void loop() {
-  //throttle = analogRead(A0);
-  
-  
-  //pct = A0/1024;
-  /*if (pct < 0.05){
-    digitalWrite(9, HIGH);
-  }*/
-  digitalWrite(8, HIGH); //Establishes forward direction of Channel A
-  digitalWrite(11, LOW);   //Disengage the Brake for Channel A
-  analogWrite(3, 255);   //Spins the motor on Channel A at full speed
+void loop(){
+  move(1, 255, 1); //motor 1, full speed, left
+
+  delay(10000); //go for 1 second
+  stop(); //stop
+  delay(250); //hold for 250ms until move again
+
+  move(1, 128, 0); //motor 1, half speed, right
+  delay(500);
+  stop();
+  delay(250);
 }
 
 
-/*forward @ full speed
-digitalWrite(12, HIGH); //Establishes forward direction of Channel A
-digitalWrite(9, LOW);   //Disengage the Brake for Channel A
-analogWrite(3, 255);   //Spins the motor on Channel A at full speed
-delay(3000);
-digitalWrite(9, HIGH); //Engage the Brake for Channel A
-delay(1000);*/
+void move(int motor, int speed, int direction){
+//Move specific motor at speed and direction
+//motor: 0 for B 1 for A
+//speed: 0 is off, and 255 is full speed
+//direction: 0 clockwise, 1 counter-clockwise
+
+  digitalWrite(STBY, HIGH); //disable standby
+
+  boolean inPin1 = LOW;
+  boolean inPin2 = HIGH;
+
+  if(direction == 1){
+    inPin1 = HIGH;
+    inPin2 = LOW;
+  }
+
+  if(motor == 1){
+    digitalWrite(AIN1, inPin1);
+    digitalWrite(AIN2, inPin2);
+    analogWrite(PWMA, speed);
+  }else{
+    digitalWrite(BIN1, inPin1);
+    digitalWrite(BIN2, inPin2);
+    analogWrite(PWMB, speed);
+  }
+}
+
+void stop(){
+//enable standby  
+  digitalWrite(STBY, LOW); 
+}
